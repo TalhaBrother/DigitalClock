8a513c2b5a655658f4bf34ea6de3b459bcf28de0
Talha Brother <talhaikram262005@gmail.com>
2025-04-18 13:17:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var hour=document.getElementById('hr');
+var minute=document.getElementById('min');
+var second=document.getElementById('sec');
+var TodaysDate=document.getElementById('date')
+var day=document.getElementById('am')
+TodaysDate.innerText=new Date();
+console.log(hour.innerText)
+
+
+setInterval(function(){
+    var currentDate=new Date();
+    if(currentDate.getHours()>12){
+        hour.innerText=currentDate.getHours()-12;
+        day.innerText="pm";
+    }
+    else if(currentDate.getHours()===0){
+        hour.innerText= 12;
+    }
+    else{
+        hour.innerText=currentDate.getHours();
+    }
+    minute.innerText=" : "+currentDate.getMinutes()+" : ";
+    second.innerText=currentDate.getSeconds();
+},1000)
